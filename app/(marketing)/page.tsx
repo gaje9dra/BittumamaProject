938a2eabf75f30a3baf1e7ea0505f9c2739a2db5
gaje9dra <126646046+gaje9dra@users.noreply.github.com>
2026-09-18 20:43:17 +0530

    feat(home): integrate organization snapshot

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,3 +1,4 @@
+import { HomeAbout } from "@/components/home/home-about";
 import { HomeAudience } from "@/components/home/home-audience";
 import { HomeCapabilities } from "@/components/home/home-capabilities";
 import { HomeCta } from "@/components/home/home-cta";
@@ -27,6 +28,7 @@ export default function Home() {
       <HomeExpertise />
       <HomeEvents />
       <HomeInsights />
+      <HomeAbout />
       <HomeCta />
     </main>
   );
