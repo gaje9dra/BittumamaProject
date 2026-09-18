ea2774c5fa13642e339ff91b1be3eb26669abf6e
gaje9dra <126646046+gaje9dra@users.noreply.github.com>
2026-09-18 16:28:58 +0530

    feat: establish next font architecture

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-body",
+});
 
 export const metadata: Metadata = {
   title: "Website Name",
@@ -13,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.variable}>{children}</body>
     </html>
   );
 }
