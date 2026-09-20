5563c1baf525fedb8ea54088d5c5f57b27ce659d
gaje9dra <126646046+gaje9dra@users.noreply.github.com>
2026-09-20 08:53:53 +0530

    Fix scroll transition wrapper structure

--- components/home/home-service-discovery.tsx
+++ components/home/home-service-discovery.tsx
@@ -67,7 +67,7 @@ export function HomeServiceDiscovery() {
                         </Link>
                       </div>
           </div>
-        </ScrollTransition>        </div>
+        </ScrollTransition>
       </Container>
     </section>
   );
